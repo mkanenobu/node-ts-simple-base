1f3745bee872f97bcecf9744e0955dfae2d05a55
mkanenobu <kaneda.1018@gmail.com>
2022-07-12 09:47:48 +0900

    add capture stdin

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-const main = () => {
+const main = async (input?: string[]): Promise<void> => {
   console.log("Hello, World!");
 };
 
 main();
+
+// const captureStdin = async (): Promise<string[]> => {
+//   const stream = (await import("readline")).createInterface({
+//     input: process.stdin,
+//   });
+//   const input: string[] = [];
+//   for await (const line of stream) {
+//     input.push(line);
+//   }
+//   return input;
+// };
